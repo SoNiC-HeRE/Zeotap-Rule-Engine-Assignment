447aa8883e0b21d098e092d2c51f528f3d4e5b1f
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2024-10-24 20:05:53 +0530

    Fix deployment errors

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ from app import create_app
 # Create an instance of the Flask application
 app = create_app()
 
+if __name__ == "__main__":
+    # Run the application in debug mode if this script is executed directly
+    app.run(debug=True)
--- wsgi.py
+++
@@ -1,5 +0,0 @@
-from app import app
-
-if __name__ == "__main__":
-    # Run the application in debug mode if this script is executed directly
-    app.run(debug=True)
